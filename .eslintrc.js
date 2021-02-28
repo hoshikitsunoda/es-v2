9fbc337347f5a916173b57e1c5f316b9c9ab4a00
hoshikitsunoda <hoshkitsunoda@gmail.com>
2021-02-28 02:19:15 -0800

    updated lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,21 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-  },
-};
+    'react/react-in-jsx-scope': 'off'
+  }
+}
